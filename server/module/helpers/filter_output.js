3499960ea39413dcd943e504b7bb9efd68091495
David Scholer <davidascholer@gmail.com>
2021-04-21 13:19:01 -0700

    ebay ad added

--- server/module/helpers/filter_output.js
+++ server/module/helpers/filter_output.js
@@ -1,21 +1,47 @@
 const filterData = data => {
     for (let d in data) {
         if (data[d]) {
-            const vendor = data[d].vendor;
-            const title = data[d].title;
-            const details = data[d].details;
-            const price = data[d].price;
-            if (vendor === 'slickdeals.net')
+            if (data[d].vendor === 'slickdeals.net'){
                 delete data[d];
-            else if (title.includes('slickdeals') || title.includes(' SD ')||title==='undefined'||title===undefined)
+                continue;
+            }
+            else if (data[d].title.includes('slickdeals') || data[d].title.includes(' SD ') || data[d].title === 'undefined' || data[d].title === undefined){
                 delete data[d];
-            else if (details.includes('slickdeals') || details.includes('thanks'))
+                continue;
+            }
+            else if (data[d].details.includes('slickdeals') || data[d].details.includes('thanks')){
                 delete data[d];
-            else if (price===undefined||price.includes('undefined'))
+                continue;
+            }
+            else if (data[d].price === undefined || data[d].price.includes('undefined')){
                 delete data[d];
+                continue;
+            }
+
+            if (data[d].vendor === 'amazon.com') {
+                data[d].details = amazon(data[d].details);
+            }
+            else if (data[d].vendor === 'ebay.com') {
+                data[d].details = ebay(data[d].details);
+            }
         }
     }
     return data;
+}
+
+const amazon = returnDetails => {
+
+    returnDetails += `<br><br><br>Don't have Amazon Prime? Students can get a free 6-Month <a
+        href='https://www.amazon.com/amazonprime?_encoding=UTF8&ascsubtag=203d87729db911eb990886ac4273965d0INT&planOptimizationId=WLPStudentMonthlyEligiblePlans&tag=vamplitude-20'>Amazon Prime</a > trial with free 2 - day shipping, unlimited video streaming & more.<br><br>Otherwise, there's also a free 1-Month <a href = 'https://www.amazon.com/amazonprime?_encoding=UTF8&tag=vamplitude-20'>Amazon Prime</a> trial available.`;
+
+    return returnDetails;
+}
+const ebay = returnDetails => {
+
+    returnDetails += `<br><br><br>Not the deal you're looking for? Ebay has a sale for <a
+        href='https://www.ebay.com/e/_electronics/direct-brands-certified-refurbished?mkcid=1&mkrid=711-53200-19255-0&siteid=0&campid=5338802310&toolid=20014&customid=&mkevt=1'>up to 50% off from name brand items</a > until June 30th.`;
+
+    return returnDetails;
 }
 
 module.exports.filterData = filterData;
